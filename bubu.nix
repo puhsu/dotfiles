eca7643eb5ee25c3d70c845699df817a4b7fb7ef
irubachev <irubachev@gmail.com>
2024-02-26 01:42:19 +0300

    revert

--- bubu.nix
+++ bubu.nix
@@ -1,6 +1,6 @@
-{...}: {
+{modulesPath, ...}: {
   # dunno if i need this
-  imports = [<nixpkgs/nixos/modules/profiles/qemu-guest.nix>];
+  imports = [(modulesPath + "/profiles/qemu-guest.nix")];
   boot.loader.grub.device = "/dev/vda";
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
   boot.initrd.kernelModules = [ "nvme" ];
