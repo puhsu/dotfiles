1fe47f0dc3c636eeafb4c002b4dfab808b036e3d
irubachev <irubachev@gmail.com>
2024-02-26 01:06:41 +0300

    sync

--- bubu.nix
+++ bubu.nix
@@ -5,6 +5,13 @@
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
 
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+    };
+  };
+
   # dummy caddy conifg
   services.caddy = {
     enabled = true;
@@ -12,4 +19,8 @@
       respond "Hello, world!"
     '';
   };
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDrz1C/Ra9cRzg2nnZvc5DthwcHPJzUykBCrWtsAO+vwucOKNMu4ONXV8hThvX6h97voLC8XA8rV9Vf9Y2Nd4yU= irubachev@irubachev-osx"
+  ];
 }
